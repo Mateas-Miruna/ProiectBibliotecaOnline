1c1a542f6f44061d2421007b87786df6028705f1
Mateas-Miruna <miruna.mateas02@e-uvt.ro>
2023-10-10 08:51:56 +0300

    initial project setup

---
+++ src/main/java/ro/uvt/info/splabmm1/Splabmm1Application.java
@@ -0,0 +1,13 @@
+package ro.uvt.info.splabmm1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Splabmm1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Splabmm1Application.class, args);
+    }
+
+}
---
+++ src/test/java/ro/uvt/info/splabmm1/Splabmm1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.uvt.info.splabmm1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Splabmm1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
